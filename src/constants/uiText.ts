78c81075f7383a974c186d1697671be2611908b1
bilelBoulhia <billel.boulahia456@gmail.com>
2025-06-09 12:58:36 +0100

    adding label constant

---
+++ src/constants/uiText.ts
@@ -0,0 +1,30 @@
+export const UI_TEXT = {
+  pages: {
+    skipSelectionPage: {
+      title: "Choose Your Skip Size",
+      description: "Select the skip size that best suits your needs",
+    },
+  },
+  stepper: {
+    postcode: "Postcode",
+    wasteType: "Waste Type",
+    selectSkip: "Select Skip",
+    permitCheck: "Permit Check",
+    chooseDate: "Choose Date",
+    payment: "Payment",
+    previous: "Previous",
+
+  },
+  drawer: {
+    title: "Selected Skip",
+    description:
+      "Imagery and information shown throughout this website may not reflect the exact shape or size specification, colors may vary, options and/or accessories may be featured at additional cost.",
+    close: "Close",
+  },
+  buttons: {
+    back: "Back",
+    continue: "Continue",
+  },
+  loading: "Loading skip options...",
+  errorRetry: "Retry",
+}; 
