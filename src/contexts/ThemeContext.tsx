0a3be929b2fe2a184d43d3c4ccb579130bea2786
bilelBoulhia <billel.boulahia456@gmail.com>
2025-06-09 12:59:09 +0100

    adding necessary contexts

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useLayoutEffect, useState, useCallback } from "react";
+import { Theme } from "../types/theme";
+import { ThemeContextProps } from "../types/theme";
+
+export const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
+
+export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    if (typeof window !== "undefined") {
+      return (localStorage.getItem("theme") as Theme) || "light";
+    }
+    return "light";
+  });
+
+ 
+  useLayoutEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = useCallback(() => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  }, []);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
